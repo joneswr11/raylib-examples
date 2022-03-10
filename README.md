84ff3d7948e33902379f40f71b450659146f8787
William Jones <William.Jones@Desktop.local>
2022-03-10 12:14:03 -0500

    added moving sprite example with collisions

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ key things to note:
 - download the release for the appropriate os and build env (for exmaple windows visual studio vs mingw)
 - add the include and lib folder in the project
 - make sure you compile against those lib files
+- if using textures, audio etc. you need to have the resources folder in the same folder as the executable
--- graphicsTest.cpp
+++ graphicsTest.cpp
@@ -10,7 +10,8 @@
 #define BALLMOVINGTEST 3
 #define ANIMATEDTEXTTEST 4
 #define BOXMOVECOLLISIONTEST 5
-#define TESTTORUN BOXMOVECOLLISIONTEST
+#define TEXTUREBOXCOLLISIONTEST 6
+#define TESTTORUN TEXTUREBOXCOLLISIONTEST
 
 void basicTest()
 {
@@ -220,7 +221,6 @@ void moveBoxWithCollisions()
 
 	InitWindow(screenWidth, screenHeight, "example - keyboard input and collision");
 
-	//Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
 	Rectangle box = { (float)screenWidth / 2, (float)screenHeight / 2, 30, 30 };
 
 
@@ -279,6 +279,102 @@ void moveBoxWithCollisions()
 
 }
 
+void moveTextureBoxWithCollisions()
+{
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	const int FPS = 60;
+	//const int MAX_FRAME_SPEED = 15;
+	//const int MIN_FRAME_SPEED = 1;
+
+	InitWindow(screenWidth, screenHeight, "raylib [texture] example - texture rectangle");
+
+	// NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+	Texture2D player = LoadTexture("resources/run.png");        // Texture loading
+
+	Vector2 position = { 350.0f, 280.0f };
+	Rectangle frameRec = { 0.0f, 0.0f, (float)player.width / 8, (float)player.height };
+	int currentFrame = 0;
+
+	int framesCounter = 0;
+	int framesSpeed = 8;            // Number of spritesheet frames shown by second
+
+	SetTargetFPS(FPS);               // Set our game to run at 60 frames-per-second
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		float movingSpeed = 2.0f;
+		// simulate running
+		if (IsKeyDown(KEY_B))
+			movingSpeed = 4.0f;
+
+		// Only able to move in one direction at a time
+		if (IsKeyDown(KEY_RIGHT)) position.x += movingSpeed;
+		else if (IsKeyDown(KEY_LEFT)) position.x -= movingSpeed;
+		else if (IsKeyDown(KEY_UP)) position.y -= movingSpeed;
+		else if (IsKeyDown(KEY_DOWN)) position.y += movingSpeed;
+
+		// makes the sprite move faster or slower along with the actual movement of the box
+		framesSpeed = (int)movingSpeed * 3;
+
+		// Make sure player does not go out of move area limits
+		if ((position.x + frameRec.width) >= GetScreenWidth()) position.x = GetScreenWidth() - frameRec.width;
+		else if (position.x <= 0) position.x = 0;
+
+		if ((position.y + frameRec.height) >= GetScreenHeight()) position.y = GetScreenHeight() - frameRec.height;
+		else if (position.y <= 0) position.y = 0;
+
+		framesCounter++;
+
+		// if the player is moving, it moves moves to the next sprite character in the file and display's it
+		if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_UP))
+		{
+			if (framesCounter >= (FPS / framesSpeed))
+			{
+				framesCounter = 0;
+				currentFrame++;
+
+				if (currentFrame > 7) currentFrame = 0;
+
+				frameRec.x = (float)currentFrame*(float)player.width / 8;
+			}
+		}
+		// if the player is not moving it reverts back to the first sprite character in the file
+		else
+		{
+			currentFrame = 0;
+			frameRec.x = (float)currentFrame*(float)player.width / 8;
+		}
+
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawTextureRec(player, frameRec, position, WHITE);  // Draw part of the texture
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	UnloadTexture(player);       // Texture unloading
+
+	CloseWindow();                // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+}
+
 int main(int argc, char** argv[])
 {
 
@@ -292,6 +388,8 @@ int main(int argc, char** argv[])
 	animatedText();
 #elif TESTTORUN==BOXMOVECOLLISIONTEST
 	moveBoxWithCollisions();
+#elif TESTTORUN==TEXTUREBOXCOLLISIONTEST
+	moveTextureBoxWithCollisions();
 #endif
 
 	return 0;
